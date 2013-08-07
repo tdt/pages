8c5bcf663aabf4ad2b7c2bd851dc210aeedf88e5
Michiel Vancoillie <info@ninetynine.be>
2013-08-07 20:41:24 +0200

    Start slash on PHP_SELF

--- src/tdt/pages/Generator.php
+++ src/tdt/pages/Generator.php
@@ -144,6 +144,7 @@ class Generator {
         if($template == 'resource'){
             $url = $this->getCurrentURI();
             $url =  preg_replace('/(\.about|\.html)$/', '', $url);
+            $url = str_replace('//', '/', $url);
             $data['json_link'] = $url . '.json';
             $data['xml_link'] = $url . '.xml';
         }
@@ -184,8 +185,6 @@ class Generator {
 
 
     private function getCurrentURI() {
-        // Start slash if PHP_SELF is empty
-        $uri = '/'. preg_replace('/index\.php$/', '', $_SERVER["PHP_SELF"]) .  $_SERVER["REQUEST_URI"];
-        return str_replace('//', '/', $uri);
+        return preg_replace('/index\.php$/', '', $_SERVER["PHP_SELF"]) .  $_SERVER["REQUEST_URI"];
     }
 }
